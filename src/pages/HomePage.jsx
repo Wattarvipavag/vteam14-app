751f64194503e2f6fb27af9fbd36d848dd223b93
Emil Lindström <lindstromemil86@gmail.com>
2024-12-09 16:01:04 +0100

    Styling, folder structures and other stuff.

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Map from "../components/Map";
+import MobileNav from "../components/MobileNav";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Map />
+      <MobileNav />
+    </div>
+  );
+}
